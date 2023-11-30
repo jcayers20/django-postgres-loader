8b1602c46dd73bba1a642f91e295fd6454fae5eb
jcayers20 <jcayers20@gmail.com>
2023-11-29 21:51:03 -0600

    Flesh out initial package structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""Package configuration details."""
+
+# built-in imports
+from setuptools import setup, find_packages
+
+setup(
+    name="django_postgres_loader",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "Django>=3.2",
+    ]
+)
